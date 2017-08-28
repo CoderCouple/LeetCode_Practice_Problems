d2fd5dfe471d22cbe3f95785ac5c2ae0d87f7b5c
Sunil Tiwari <sunil28071987@gmail.com>
2017-08-28 12:13:31 -0700

    Update and rename 101 SymmetricTree_Iterative.java to 101 SymmetricTree.java

--- Top100LikedQuestions/101 SymmetricTree.java
+++ Top100LikedQuestions/101 SymmetricTree.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
-public class SymmetricTree_Iterative {
+public class SymmetricTree {
 
 	public static boolean isSymmetric(TreeNode root) {
 		Queue<TreeNode> q = new LinkedList<TreeNode>();
@@ -31,6 +31,18 @@ public class SymmetricTree_Iterative {
 		return true;
 	}
 
+	public static boolean isSymmetric_rec(TreeNode root) {
+		return isMirror(root, root);
+	}
+
+	public static boolean isMirror(TreeNode t1, TreeNode t2) {
+		if (t1 == null && t2 == null)
+			return true;
+		if (t1 == null || t2 == null)
+			return false;
+		return (t1.val == t2.val) && isMirror(t1.right, t2.left) && isMirror(t1.left, t2.right);
+	}
+
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(1);
 		TreeNode left = new TreeNode(2);
@@ -40,6 +52,7 @@ public class SymmetricTree_Iterative {
 		root.right = right;
 
 		System.out.println(isSymmetric(root));
+		System.out.println(isSymmetric_rec(root));
 	}
 
 }
